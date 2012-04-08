4513b2bd12fd0a2e6779a18b63f20ab798fabed8
Rami Chowdhury <rami.chowdhury@gmail.com>
2012-04-08 16:22:05 +0100

    moved scraper to its own NS

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject ldnpyvideo "0.1.0-SNAPSHOT"
   :description "Web scraper and web site for London PyCon By Video"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [enlive "1.0.0"]
-                 [jayq "0.1.0-SNAPSHOT"]
-                 [fetch "0.1.0-SNAPSHOT"]
-                 [noir "1.3.0-alpha1"]]
+                 [noir "1.3.0-alpha1"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [org.clojure/java.jdbc "0.1.1"]]
   :plugins [[lein-cljsbuild "0.1.2"]]
   :cljsbuild
   {:builds
