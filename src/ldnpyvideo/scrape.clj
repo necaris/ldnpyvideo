7587039af92e3f6cff4a98eda0905f6783ccc8ed
Rami Chowdhury <rami.chowdhury@gmail.com>
2012-04-09 00:17:58 +0100

    reshuffled some code in scrape

--- src/ldnpyvideo/scrape.clj
+++ src/ldnpyvideo/scrape.clj
@@ -87,7 +87,7 @@
     (sql/with-connection db-url
       (apply sql/insert-records :data formatted-recs))))
 
-(defn -main
+(defn refresh-all
   []
   (let [listing-page (fetch-url (str video-root video-cat))]
     ;; (println "Dropping any existing data")
@@ -104,3 +104,7 @@
      (map get-talk-data (get-talk-urls listing-page)))
     ;; (println "Inserted")
     ))
+
+(defn -main
+  []
+  (refresh-all))
