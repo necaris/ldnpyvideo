419f64ee082a40e6b709a19d9c43cbcceb00047f
Rami Chowdhury <rami.chowdhury@gmail.com>
2012-04-09 12:40:19 +0100

    elaborated notice, removed from leaderboard

--- src/ldnpyvideo/template.clj
+++ src/ldnpyvideo/template.clj
@@ -13,6 +13,9 @@
   [:button.vid-vote-btn] (if (nil? (get context :ldr))
                            (html/set-attr :id (str "video-btn-" (:id context)))
                            (fn [node] {:tag :p}))
+  [:p#notice] (if (nil? (get context :ldr))
+                identity
+                (fn [node] nil))
   )
 
 (html/deftemplate index "templates/main.html"
